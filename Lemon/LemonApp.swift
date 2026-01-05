1d328a12a084607d050ec8eda7cf6f576cf518d9
ShrimpMolly <dh22667@my.westlake.school.nz>
2026-01-06 00:10:02 +0530

    Initial Commit

---
+++ Lemon/LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonApp.swift
+//  Lemon
+//
+//  Created by Shaluka Hewapatha on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
